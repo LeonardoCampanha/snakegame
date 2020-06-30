dc95040cbef022011efde4e3eaf727efcc4b51f1
Leonardo Campanha <ileonardo@outlook.com>
2020-06-30 14:36:56 +0100

    moving snake

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const snake_border = 'darkblue'
 
 
 let snake = [{ x: 200, y: 200 }, { x: 190, y: 200 }, { x: 180, y: 200 }, { x: 170, y: 200 }, { x: 160, y: 200, }];
-
+//Horizontal velocity
+let dx = 10;
+//Vertical velocity
+let dy = 0;
 //Get the canvas element
 const snakeboard = document.getElementById("snakeboard");
 //return a two dimensional drawing context
@@ -16,11 +19,18 @@ start();
 
 //main function called repeatedly to keep the game running
 function start() {
-    clearCanvas();
-    drawSnake();
+    setTimeout(function onTick() {
+
+        clear_board();
+        move_snake();
+        drawSnake();
+        //call start again
+        start();
+    }, 100)
+
 }
 
-function clearCanvas() {
+function clear_board() {
     //  Select the colour to fill the drawing
     snakeboard_ctx.fillStyle = board_background;
     //  Select the colour for the border of the canvas
@@ -50,3 +60,11 @@ function drawSnakePart(snakePart) {
     snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
+function move_snake() {
+    //Create the new Snake's head
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy};
+   // add the new head to the beginning of snkae body
+    snake.unshift(head);
+    snake.pop();
+
+}
